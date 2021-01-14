230ffcd0f85b9e85fd322cfab751dd5f921ab6ec
Denys Hryshko <gryshko.denys@gmail.com>
2021-01-14 12:00:17 +0200

    version that working before refactoring

--- vue.js
+++ vue.js
@@ -1,11 +1,8 @@
-// вы можете как угодно изменять программу и код
-// добавлять любые переменные и модели
-// ваша задача реализовать так, как показано на видео, чтобы оно работало
-
 const App = {
   data() {
     return {
-      activeIndex: 0, // то, что позволяет определить текущий активный шаг
+      activeIndex: 0,
+      isStartOverButtonActive: false,
       steps: [
         {title: 'Основы', text: 'В блоке вы познакомитесь со всеми основами Vue.js на практике. На протяжении блока мы напишем реактивное приложение, в процессе разработки которого разберем вся базу фреймворка.'},
         {title: 'Компоненты', text: 'Один из самых важных блоков в курсе, где вы узнаете все о компонентах. В блоке мы напишем 2 разных приложения и создадим более 5 различных UI компонентов как в реальной разработке. Блок расскажет про абсолютно все составляющие, которые есть в компонентах: взаимодействие, slots, асинхронные и динамические компоненты и тонна примеров.'},
@@ -17,19 +14,23 @@ const App = {
   },
   methods: {
     prev() {
-      this.activeIndex--
-      // когда нажимаем кнопку назад
+      this.isStartOverButtonActive 
+        ? this.activeIndex = 0
+        : this.activeIndex--
     },
+
     reset() {
       this.activeIndex = 0
-      // начать заново
     },
-    nextOfFinish() {
-      this.activeIndex++
-      // кнопка вперед или закончить
+
+    nextStep() {
+      this.activeIndex === 4 
+        ? this.isStartOverButtonActive = true
+        : this.activeIndex++
     },
-    setActive(idx) {
-      // когда нажимаем на определенный шаг
+
+    setActive(index) {
+      this.activeIndex = index
     }
   },
   computed: {
@@ -43,11 +44,19 @@ const App = {
 
     isStepDone() {
       return this.currentStep
+    },
+
+    isForwardButtonShown() {
+      return this.activeIndex < this.steps.length
+    },
+
+    textOfLeftButton() {
+      return this.isStartOverButtonActive ? 'Начать заново' : 'Назад'
+    },
+
+    textOfRightButton() {
+      return this.activeIndex === 4 ? 'Закончить' : 'Вперед'
     }
-    // тут стоит определить несколько свойств:
-    // 1. текущий выбранный шаг
-    // 2. выключена ли кнопка назад
-    // 3. находимся ли мы на последнем шаге
   }
 }
 
